14d8f95ecba1c67dae82c2cb9c1eced9216be056
sujeet <sujeetkrgupta9122@gmail.com>
2024-06-27 23:43:18 +0530

    Test for superUser add

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL="core.User"
